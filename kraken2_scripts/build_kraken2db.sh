6b69b7fbf40a6efdf9f1a7befe79a70ab3f60ddc
Quynh Nhu Nguyen <118354457+lucianhu@users.noreply.github.com>
2024-09-02 23:47:58 +0200

    Update build_kraken2db.sh

--- kraken2_scripts/build_kraken2db.sh
+++ kraken2_scripts/build_kraken2db.sh
@@ -15,11 +15,12 @@ wget https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20240605.tar.gz
 tar -xvf k2_viral_20240605.tar.gz -C virus_db
 
 # Fungi
-## Download the NCBI taxonomy data required for the Kraken2 database construction in the "fungi_db" directory
+# Download the NCBI taxonomy data required for the Kraken2 database construction 
+# and store it in the "fungi_db" directory
 kraken2-build --download-taxonomy --db fungi_db
 
-## Build the Kraken2 database for fungi using the downloaded taxonomy in the "fungi_db" directory
-kraken2-build --build --db fungi_db
+# Download the fungal library sequences from NCBI and add them to the "fungi_db" directory
+kraken2-build --download-library fungi --db fungi_db
 
-## Add custom fungal sequences from "fungi.fasta" to the "fungi_db" database
-kraken2-build --add-to-library fungi.fasta --db fungi_db
+# Build the Kraken2 database for fungi using the downloaded taxonomy and library data
+kraken2-build --build --db fungi_db
